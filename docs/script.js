dce8e4fcc4bbd341a2220542eb59fa2375c47e8e
David Megginson <david.megginson@gmail.com>
2020-05-09 19:19:00 -0400

    Added numbered markers and separate route page.

--- docs/script.js
+++ docs/script.js
@@ -117,9 +117,11 @@ fdel.updateMap = function () {
                     if (latlon === false) {
                         alert("Cannot find address " + entry.address);
                     } else {
-                        var marker = L.marker(latlon, { title: fullAddress });
+                        var icon = new L.AwesomeNumberMarkers({"number": i+1});
+                        var marker = L.marker(latlon, { title: fullAddress, icon: icon });
                         marker.addTo(fdel.markerLayer);
-                        fdel.map.fitBounds(fdel.markerLayer.getBounds());
+                        var bounds = fdel.markerLayer.getBounds();
+                        fdel.map.fitBounds(bounds.pad(0.25));
                     }
                 });
             };
@@ -135,15 +137,25 @@ fdel.updateMap = function () {
  */
 fdel.rebuildForm = function () {
     fdel.formNode.innerHTML = "";
-    for (var i = 0; i < fdel.data.entries.length; i++) {
-        var entry = fdel.data.entries[i];
+
+    if (fdel.data.entries.length == 0) {
+        fdel.data.entries = [
+            {
+                "quantity": 1,
+                "address": "",
+                "notes": ""
+            }
+        ];
+    }
+
+    fdel.data.entries.forEach((entry, i) => {
         var row = fdel.rowTemplate.cloneNode(true);
         var fields = row.getElementsByTagName("input");
         for (var j = 0; j < fields.length; j++) {
             fields[j].value = entry[fields[j].name];
         }
         fdel.formNode.appendChild(row);
-    }
+    });
 };
 
 
@@ -178,6 +190,39 @@ fdel.removeRow = function (node) {
     }
 };
 
+fdel.displayRoute = function () {
+
+    if (fdel.data.entries.length == 0) {
+        fdel.routeNode.textContent = "No delivery addresses defined!";
+    }
+    
+    fdel.data.entries.forEach((entry, i) => {
+        var itemNode = document.createElement("li");
+        var child;
+
+        if (entry.address) {
+            child = document.createElement("p");
+            child.setAttribute("class", "address");
+            child.textContent = entry.address;
+            itemNode.appendChild(child);
+
+            child = document.createElement("p");
+            child.setAttribute("class", "quantity");
+            child.textContent = "Deliver " + entry.quantity + " box(es)";
+            itemNode.appendChild(child);
+
+            if (entry.notes) {
+                child = document.createElement("p");
+                child.setAttribute("class", "notes");
+                child.textContent = entry.notes;
+                itemNode.appendChild(child);
+            }
+            
+            fdel.routeNode.appendChild(itemNode);
+        }
+    });
+};
+
 
 /**
  * Update data whenever the user leaves a form field.
@@ -195,20 +240,27 @@ fdel.doChange = function (node) {
         fdel.updateMap();
     }
     window.location.hash = JSON.stringify(fdel.data);
-    fdel.linkNode.setAttribute("href", window.location.hash);
+    fdel.linkNode.setAttribute("href", "route.html" + window.location.hash);
     return true;
 };
 
 
+fdel.setupCommon = function () {
+    // Check for saved data in the URL
+    if (window.location.hash) {
+        fdel.data = JSON.parse(decodeURIComponent(window.location.hash.substring(1)));
+    }
+};
+
 /**
  * Set up once the document finishes loading.
  */
-window.onload = function () {
+fdel.setupEdit = function () {
     fdel.formNode = document.getElementById("deliveries");
     fdel.formNode.addEventListener("submit", fdel.doChange);
 
     fdel.linkNode = document.getElementById("share");
-    fdel.linkNode.setAttribute("href", window.location.hash);
+    fdel.linkNode.setAttribute("href", "route.html" + window.location.hash);
 
     // Add change handler to all form inputs
     var fields = fdel.formNode.getElementsByTagName("input");
@@ -223,12 +275,17 @@ window.onload = function () {
     // Grab a template for a row (delivery)
     fdel.rowTemplate = document.getElementsByTagName("fieldset")[0].cloneNode(true);
 
-    // Check for saved data in the URL
-    if (window.location.hash) {
-        fdel.data = JSON.parse(decodeURIComponent(window.location.hash.substring(1)));
-        fdel.rebuildForm();
-    }
-
-    // Set up map
+    fdel.setupCommon();
+    fdel.rebuildForm();
     fdel.setupMap();
 };
+
+
+fdel.setupRoute = function () {
+    fdel.setupCommon();
+    fdel.routeNode = document.getElementById("route-node");
+    fdel.displayRoute();
+    fdel.setupMap();
+};
+
+
